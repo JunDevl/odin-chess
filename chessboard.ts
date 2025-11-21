7143640f50c4875c14478abd4bbaeb9f19e909b4
JunDevl <juninhoplay.pedone@gmail.com>
2025-11-20 23:18:54 -0300

    Modified the dfs algorithm, still wip

--- chessboard.ts
+++ chessboard.ts
@@ -1,55 +1,65 @@
-import { start } from "repl";
 import { Queue } from "./queue";
-import { visitParameterList } from "typescript";
 
 const maxBoardWidth = 7;
 
-const possibleKnightMoves = [
-  [-2, 1], 
-  [-1, 2], 
-  [1, 2], 
-  [2, 1], 
-  [2, -1], 
-  [1, -2], 
-  [-1, -2], 
-  [-2, -1]
-]
+class ChessSquare {
+  static discoveredSquares = new Map<string, ChessSquare>();
 
-function shortestKnightPath(startCoordinate: [number, number], 
-                            endCoordinate: [number, number], 
-                            queue?: Queue<[number, number]>, 
-                            visitedCoordinates?: [[number, number]]): [[number, number]] | undefined {
+  static readonly isImpossibleMove = (position: [number, number], moveTo: [number, number]): boolean => {
+    const [posX, posY] = position;
+    const [moveX, moveY] = moveTo;
 
-  let resultPath: [[number, number]] | undefined = undefined;
-
-  const [posx, posy] = startCoordinate;
-  const [endx, endy] = endCoordinate;
-  visitedCoordinates = visitedCoordinates ? visitedCoordinates : [[posx, posy]];
-
-  for (let move of possibleKnightMoves) {
-    //console.log(startCoordinate);
-    const [movex, movey] = move;
-    const [nextx, nexty] = [posx + movex, posy + movey]
-
-    if (nextx < 0 || nexty < 0) continue;
-
-    if (nextx === endx && nexty === endy) {
-      visitedCoordinates.push([nextx, nexty]);
-      resultPath = visitedCoordinates;
-      break;
-    };
-
-    if (!queue) queue = new Queue()
-    else if (queue.Size > 0) queue.dequeue();
-
-    if (visitedCoordinates.length > 1) visitedCoordinates.push([posx, posy]);
-  
-    queue.enqueue([nextx, nexty]);
+    return (posX + moveX < 0 || posX + moveX > maxBoardWidth || posY + moveY < 0 || posY + moveY > maxBoardWidth);
   }
+  static readonly knightMoves = [
+    [-2, 1], [-1, 2], [1, 2], [2, 1], 
+    [2, -1], [1, -2], [-1, -2], [-2, -1]
+  ]
+  readonly possibleMoves: [[number, number]] | null = null;
 
-  if (!queue || !queue.first) return resultPath;
+  predecessor: ChessSquare | null = null;
+  readonly position: [number, number] | null = null;
 
-  return resultPath ?? shortestKnightPath(queue.first?.data, endCoordinate, queue, visitedCoordinates);
+  constructor(...position: [number, number]) {
+    this.position = position;
+    const [posX, posY] = position;
+
+    ChessSquare.discoveredSquares.set(`${posX}${posY}`, this);
+
+    for (const move of ChessSquare.knightMoves as [[number, number]]) {
+      const [moveX, moveY] = move;
+
+      if (ChessSquare.isImpossibleMove(position, move))
+        continue;
+
+      if (this.possibleMoves) this.possibleMoves.push(position);
+      else this.possibleMoves = [position];
+    }
+  }
+}
+
+function shortestKnightPath(startPosition: [number, number], 
+                            endPosition: [number, number]): any {
+
+  const initialSquare = new ChessSquare(...startPosition);
+  const finalSquare = new ChessSquare(...endPosition);
+
+  const searchQueue = [finalSquare];
+  while (!searchQueue.includes(initialSquare)) {
+    const currentSquare = searchQueue.shift() as ChessSquare;
+
+    const [curX, curY] = currentSquare?.position as [number, number];
+
+    for (let possibleMove of currentSquare?.possibleMoves!) {
+      const [moveX, moveY] = possibleMove;
+      const [nextX, nextY] = [curX + moveX, curY + moveY];
+
+      if (ChessSquare.isImpossibleMove(currentSquare?.position as [number, number], possibleMove)) continue;
+
+      const nextSquare = ChessSquare.discoveredSquares.get(`${nextX}${nextY}`) ?? new ChessSquare(nextX, nextY);
+      nextSquare.predecessor = currentSquare;
+    }
+  }
 }
 
 console.log("Shortest path:");
